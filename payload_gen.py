3c9f030c66db859410560ea24fe41de25b8d0aff
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2023-03-02 09:24:17 -0500

    Add missing parentheses
    eval() takes a string of code to be evaluated, and the parentheses were missing

--- payload_gen.py
+++ payload_gen.py
@@ -5,12 +5,12 @@ def generateVEXPayload(input):
     for char in input:
         ascii.append(ord(char))
     
-    print("eval(", end="")                   # eval() contains chars only within (Rachael Vex)
+    print("eval(\"", end="")                   # eval() contains chars only within (Rachael Vex)
     for char_code in ascii[:-1]:             # for all char codes but the last
         print(f"chr({char_code})+", end="")
 
     print(f"chr({ascii[-1]})", end="")       # no concat for last char
-    print(")", end="")
+    print("\")", end="")
 
 if __name__ == "__main__":
     line = input("Enter a line of python: ") 
